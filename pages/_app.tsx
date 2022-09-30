e795dac1b8e193c9f619a4441d26e0b2f79f5750
pat <patgiok@gmail.com>
2022-09-30 16:14:01 -0700

    added session to page props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { Session } from "next-auth";
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
-  
-  return(
-    <SessionProvider session={session}>
-     <Component {...pageProps} />
+
+function MyApp({ Component, pageProps, }: AppProps<{ session: Session; }>) {
+
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
     </SessionProvider>
   )
 }
