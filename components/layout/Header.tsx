f7f4f627ae43f7c20e6379f9e18be29be61590aa
pat <patgiok@gmail.com>
2022-10-01 10:30:54 -0700

    added graphql, apollo-server-micro, micro-cors pkgs.  Removed extra styles, extra imports index

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { signIn, signOut, useSession } from 'next-auth/react'
 
-
 export const Header = () => {
     const { data: session } = useSession();
     console.log(session)
+
     if(!session) {
         return (
             <button onClick={() => signIn()}>Sign In</button>
         )
     }
+    
   return (<>
     <div> signed in as {session.user!.email}</div>
 
